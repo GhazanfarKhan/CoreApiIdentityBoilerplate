75bd46f27fc0b05a32f9ffe9b2051eba5f331089
Ghazanfar Khan <mghazanfar@hotmail.co.uk>
2020-06-07 00:46:23 +0500

    code checkin

---
+++ LFSAPI/Services/TokenService.cs
@@ -0,0 +1,52 @@
+﻿using LFSAPI.Entities;
+using LFSAPI.Responses;
+using LFSAPI.Settings;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFSAPI.Services
+{
+    public class TokenService
+    {
+        private readonly JWTSettings jwtSettings;
+        public TokenService(JWTSettings _jwtSettings)
+        {
+            jwtSettings = _jwtSettings;
+        }
+        public string GenerateToken(LoginResponse user)
+        {
+            var claims = BuildClaims(user);
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.JwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(jwtSettings.JwtExpireDays));
+
+            var token = new JwtSecurityToken(
+                jwtSettings.JwtIssuer,
+                jwtSettings.JwtIssuer,
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private List<Claim> BuildClaims(LoginResponse user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("Email", user.Email),
+                new Claim("FirstName", user.FirstName),
+                new Claim("LastName", user.LastName),
+                new Claim("Role", user.Role),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+            return claims;
+        }
+    }
+}
